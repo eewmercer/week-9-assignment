4de0872bc2c66d7ef9af58c808a2eebe597c532a
Emmaline Mercer <eewmercer@gmail.com>
2024-11-01 23:20:18 -0400

    Fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="heading">
       <h1>Emmaline's Week 9 Assignment</h1>
     </div>
   );
