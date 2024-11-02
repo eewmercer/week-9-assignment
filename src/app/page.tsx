d4ae7ebd2ac83aea2db22172ac5ecf0b7add5b0c
Emmaline Mercer <eewmercer@gmail.com>
2024-11-01 23:30:02 -0400

    Fix apostrophy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="heading">
-      <h1>Emmaline's Week 9 Assignment</h1>
+      <h1>Emmaline: Week 9 Assignment</h1>
     </div>
   );
 }
