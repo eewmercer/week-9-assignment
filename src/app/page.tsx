2467481ad8917d2e716061560d770af921dbb28a
Emmaline Mercer <eewmercer@gmail.com>
2024-11-02 17:23:41 -0400

    Fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ const prisma = new PrismaClient();
 
 export default async function Home() {
   const videos = await prisma.video.findMany();
-  const videosEl = videos.map((video) => <li>{video.name}</li>)
+  const videosEl = videos.map((video) => <li key={video.id}>{video.name}</li>)
 
   return (
     <div className="heading">
       <h1>Emmaline: Week 9 Assignment</h1>
-      {videosEl}
+      <div className="videos-list">
+        {videosEl}
+      </div>
     </div>
   );
 }
