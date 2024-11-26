011702e2a6a973ed04922a4c1f74623b0d7b50dd
Emmaline Mercer <eewmercer@gmail.com>
2024-11-26 18:46:03 -0500

    fix

--- src/app/videos/videosMethods.js
+++ src/app/videos/videosMethods.js
@@ -2,8 +2,8 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-// export default async function videosMethods(formData) {
-    let deleteVideo = await prisma.video.delete({
+export default async function deleteVideo(formData) {
+    await prisma.video.delete({
         where: {id: Number(formData.get('videoID'))}
     })
-// }
+}
